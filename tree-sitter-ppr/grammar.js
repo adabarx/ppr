2abd3f73298111af02d6e67851012c8243d70a0d
Adamina Barx <adaminabarx@gmail.com>
2024-12-25 19:20:47 -0600

    init

---
+++ tree-sitter-ppr/grammar.js
@@ -0,0 +1,81 @@
+/**
+ * @file parser for paper—ppr—projects
+ * @author Adamina Barx <adaminabarx@gmail.com>
+ * @license MIT
+ */
+
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+module.exports = grammar({
+  name: "ppr",
+
+  rules: {
+    comp_set_var: $ => seq('@', /[a-zA-Z\d]+/, '=', '"', seq(repeat1(choice($.word, $.punctuation))), '"', '\n'),
+
+    comp_get_var: $ => seq('@', /[a-zA-Z\d]+/),
+
+    bookmark: $ => seq(
+      '[',
+      choice(
+        repeat1($.word),
+        $.bookmark,
+      ),
+      ']'
+    ),
+
+    bold: $ => seq('**', seq(repeat1(choice($.word, $.punctuation))), '**'),
+
+    italic: $ => seq('*', seq(repeat1(choice($.word, $.punctuation))), '*'),
+
+    word: $ => repeat1(/[a-zA-Z\d']+/),
+
+    sentence: $ => seq(repeat1(choice(
+      $.word,
+      $.semicolon,
+      $.colon,
+      $.comma,
+      $.question_mark,
+      $.exclaimation_point,
+      $.emdash,
+    )), $.period),
+
+    paragraph: $ => seq(repeat1($.sentence), '\n'),
+
+    title: $ => seq('#', '!', $.paragraph),
+
+    heading: $ => seq('#', repeat('#'), $.paragraph),
+
+    footnote: $ => seq(
+      '{',
+      seq(repeat1(choice($.word, $.punctuation))),
+      '}', '^', '{',
+      seq(repeat1(choice($.word, $.punctuation))),
+      '}',
+    ),
+
+    punctuation: $ => choice(
+      $.semicolon,
+      $.colon,
+      $.comma,
+      $.period,
+      $.question_mark,
+      $.exclaimation_point,
+      $.emdash,
+    ),
+
+    semicolon: $ => ';',
+
+    colon: $ => ':',
+
+    comma: $ => ',',
+
+    period: $ => '.',
+
+    question_mark: $ => '?',
+
+    exclaimation_point: $ => '!',
+
+    emdash: $ => '—',
+  }
+});
