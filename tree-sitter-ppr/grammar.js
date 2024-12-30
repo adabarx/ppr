3ecc7a4f23c04c903066c77ff7e262c02b478903
Adamina Barx <adaminabarx@gmail.com>
2024-12-29 20:14:02 -0600

    new grammar

--- tree-sitter-ppr/grammar.js
+++ tree-sitter-ppr/grammar.js
@@ -11,102 +11,128 @@ module.exports = grammar({
   name: "ppr",
 
   rules: {
-    comp_set_var: $ => seq('@', /[a-zA-Z\d]+/, '=', seq(repeat1(choice($.word, $.punctuation))), '\n'),
+    document: $ => repeat(choice(
+      $.function,
+      $.title,
+      $.heading,
+      $.bookmark,
+      $.paragraph,
+    )),
 
-    comp_get_var: $ => seq('@', /[a-zA-Z\d]+/),
+    title: $ => seq('\\', 'T',
+      seq(
+        repeat($.sentence),
+        optional($.fragment),
+      )
+    ), 
+    heading: $ => seq('\\', choice($.h1, $.h2, $.h3, $.h4, $.h5, $.h6),
+      seq(
+        repeat($.sentence),
+        optional($.fragment),
+      )
+    ),
+    bookmark: $ => seq('\\', 'B',
+      seq(
+        repeat($.sentence),
+        optional($.fragment),
+      )
+    ), 
+    paragraph: $ => seq('\\', 'P', repeat($.sentence), optional($.fragment)),
 
-    bookmark: $ => seq(
-      '[',
-      choice(
-        $._content,
-        $.bookmark,
-      ),
-      ']'
+    identifier: $ => seq(/[a-z]/, /[a-zA-Z\d]*/),
+    parameter: $ => seq(/[a-z]/, /[a-zA-Z\d]*/),
+    value: $ => seq('"', /[^"]+/, '"'),
+    function: $ => seq('\\', $.identifier, '(', repeat(seq($.parameter, ':', $.value, ',')), optional(seq($.parameter, ':', $.value)), ')'),
+
+    //word: $ => /[a-zA-Z'\d]+(-[a-zA-Z'\d]+)*/,
+    word: $ => /[a-zA-Z'\d]+/,
+    hyphen: $ => seq($.word, repeat1(seq('-', $.word))),
+
+    style_marker: $ => choice(
+      $.bold,
+      $.italics,
+      $.underline,
+      $.reference,
     ),
 
-    bold: $ => seq(
-      '**',
-      $._content,
-      '**'
-    ),
-
-    italic: $ => seq(
-      '//', 
-      $._content,
-      '//'
-    ),
-
-    underline: $ => seq(
-      '__',
-      $._content,
-      '__'
-    ),
-
-    strikethrough: $ => seq(
-      '--',
-      $._content,
-      '--'
-    ),
-
-    word: $ => /[a-zA-Z\d']+/,
-
-    linked_word: $ => seq(repeat1(seq($.word, '-')), $.word),
-
-    sentence: $ => seq(
-      repeat1(choice(
-        $.word,
-        $.linked_word,
-        $.punctuation,
-      )),
-      $.sentence_end
-    ),
-
-    paragraph: $ => seq(repeat1($.sentence), '\n'),
-
-    title: $ => seq('#', '!', $._content),
-
-    heading: $ => seq(repeat1('#'), $._content),
-
-    footnote: $ => seq(
-      '{',
-      $._content,
-      '}', '^', '{',
-      $._content,
-      '}',
-    ),
-
-    sentence_end: $ => choice(
+    sentence: $ => prec(3, seq(repeat1(choice(
+      $.hyphen,
+      $.word,
+      $.punctuation,
+      $._surround,
+      $.style_marker,
+      $.other,
+    )), $.end)),
+    
+    fragment: $ => prec(2, repeat1(choice(
+      $.hyphen,
+      $.word,
+      $.punctuation,
+      $._surround,
+      $.style_marker,
+      $.other,
+    ))),
+    
+    end: $ => choice(
       $.period,
       $.question_mark,
       $.exclamation_point,
     ),
 
     punctuation: $ => choice(
+      $.comma,
       $.semicolon,
       $.colon,
-      $.comma,
-      $.emdash,
+      $.fslash,
+      $.emdash
     ),
+
+    _surround: $ => choice(
+      $.quote,
+      $.parentheses,
+    ),
+
+    quote: $ => prec.left(seq("\"", repeat($.sentence), optional($.fragment), "\"")),
+
+    parentheses: $ => prec.left(seq("(", repeat($.sentence), optional($.fragment), ")")),
+
+    other: $ => choice('@', '$', '%', '^', '&', '*', '&', '_', '+', '=', '~', '`'),
+
+    h1: $ => 'H1',
+
+    h2: $ => 'H2',
+
+    h3: $ => 'H3',
+
+    h4: $ => 'H4',
+
+    h5: $ => 'H5',
+
+    h6: $ => 'H6',
 
     period: $ => '.',
 
-    question_mark: $ => '?',
-
-    exclamation_point: $ => '!',
+    comma: $ => ',',
 
     semicolon: $ => ';',
 
     colon: $ => ':',
 
-    comma: $ => ',',
+    fslash: $ => '/',
 
-    emdash: $ => '-',
+    question_mark: $ => '?',
 
-    _content: $ => seq(repeat1(choice(
-      $.word,
-      $.linked_word,
-      $.punctuation,
-      $.sentence_end,
-    ))),
+    exclamation_point: $ => '!',
+
+    bold: $ => '**',
+
+    italics: $ => '//',
+
+    emdash: $ => '--',
+
+    underline: $ => '__',
+
+    reference: $ => '&&',
+
   }
 });
